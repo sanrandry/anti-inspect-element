9310382fd74a61b8f8357ea2286b6292340df197
san randry <sinrandry@gmail.com>
2020-03-17 15:17:14 +0300

    first commit

---
+++ anti-inspect-element.js
@@ -0,0 +1,33 @@
+/**
+ * @author san randry
+ * @license MIT license
+ * this plugin disable all inspect element feature on a web browser
+ */
+// disable right click
+document.oncontextmenu = function (e) {
+    e.preventDefault()
+    console.log(e)
+}
+// disable somme additional key
+document.onkeydown = function (e) {
+    // disable f12
+    if (event.keyCode == 123) {
+        return false;
+    }
+    // disable ctrl+shift+I
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+C
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+j
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    // disable ctrl+shift+U
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
